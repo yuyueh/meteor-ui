cea48b85e97df7c6e7ef722ade27c9e8ebd24dee
DESKTOP-DLEJLUF\yuyue <yuyueh.chang@gmail.com>
2020-11-28 11:32:52 +0800

    create alert component

---
+++ src/components/alert/alert.tsx
@@ -0,0 +1,51 @@
+import classNames from 'classnames';
+import React, { useState } from 'react';
+
+export enum AlertType {
+    Success = 'success',
+    Default = 'default',
+    Danger = 'danger',
+    Warning = 'warning',
+}
+
+interface IAlertProps {
+    alertType?: AlertType;
+    title: string;
+    description?: string;
+    onClose?: Function;
+}
+
+const Alert: React.FC<IAlertProps> = ({
+    alertType,
+    title,
+    description,
+    onClose,
+}) => {
+    const [closed, setClosed] = useState(false);
+    const classes = classNames('mt-alert', {
+        [`mt-alert-${alertType}`]: alertType,
+    });
+    const handleClose = (e: React.MouseEvent<HTMLButtonElement>) => {
+        setClosed(true);
+        onClose?.(e);
+    };
+
+    return closed ? null : (
+        <span className={classes}>
+            <div className="mt-alert-title">{title}</div>
+            {description && (
+                <div className="mt-alert-description">{description}</div>
+            )}
+            <button
+                className="mt-alert-close-icon"
+                onClick={handleClose}
+            ></button>
+        </span>
+    );
+};
+
+Alert.defaultProps = {
+    alertType: AlertType.Default,
+};
+
+export default Alert;
